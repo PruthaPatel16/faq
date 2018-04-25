847e01add754115e845fa76c416a6052debed39c
prutha <pruthap416@gmail.com>
2018-04-24 22:32:18 -0400

    modification

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,128 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+namespace App\Http\Controllers;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Answer;
+use App\Question;
+use Illuminate\Support\Facades\Auth;
 
-Auth::routes();
+use Illuminate\Http\Request;
 
-Route::get('/home', 'HomeController@index')->name('home');
+class AnswerController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($question)
+    {
+        $answer = new Answer;
+        $edit = FALSE;
+        return view('answerForm', ['answer' => $answer,'edit' => $edit, 'question' =>$question  ]);
+    }
 
-Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $question)
+    {
 
-Route::get('/question/{question_id}/answer/{answer_id}', 'AnswerController@show')->name('answer.show');
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
 
-Route::resources([
-    'question' => 'QuestionController',
-]);
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+
+        ]);
+        $input = request()->all();
+        $question = Question::find($question);
+        $Answer = new Answer($input);
+        $Answer->user()->associate(Auth::user());
+        $Answer->question()->associate($question);
+        $Answer->save();
+
+        return redirect()->route('questions.show',['question_id' => $question->id])->with('message', 'Saved');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($question,  $answer)
+    {
+        $answer = Answer::find($answer);
+
+        return view('answer')->with(['answer' => $answer, 'question' => $question]);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($question,  $answer)
+    {
+        $answer = Answer::find($answer);
+        $edit = TRUE;
+        return view('answerForm', ['answer' => $answer, 'edit' => $edit, 'question'=>$question ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $question, $answer)
+    {
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+
+        ]);
+
+        $answer = Answer::find($answer);
+        $answer->body = $request->body;
+        $answer->save();
+
+        return redirect()->route('answers.show',['question_id' => $question, 'answer_id' => $answer])->with('message', 'Updated');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($question, $answer)
+    {
+        $answer = Answer::find($answer);
+
+        $answer->delete();
+        return redirect()->route('questions.show',['question_id' => $question])->with('message', 'Delete');
+
+    }
+}
